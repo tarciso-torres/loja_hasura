9bef1802e18a04cb1885eaa1db8a5f2cbba1d092
Tarciso Torres <tarcisoftorres@gmail.com>
2020-02-03 08:31:19 -0300

    Adicionado elementos de fundo na tela de cadastro de produto

--- lib/app/modules/add_produto/add_produto_page.dart
+++ lib/app/modules/add_produto/add_produto_page.dart
@@ -16,79 +16,116 @@ class _AddProdutoPageState extends State<AddProdutoPage> {
   @override
   Widget build(BuildContext context) {
     var primaryColor = Theme.of(context).primaryColor;
+    var mediaQueryHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            LabelWidget(
-              title: "Descrição",
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: mediaQueryHeight - 250,
+            right: -50,
+            child: CircleAvatar(
+              radius: 130,
+              backgroundColor: Theme.of(context).primaryColor.withOpacity(.4)
             ),
-            TextFieldWidget(
-              hintText: "Descrição do Produto",
+          ),
+          Positioned(
+            top: mediaQueryHeight - 200,
+            right: 50,
+            child: CircleAvatar(
+              radius: 130,
+              backgroundColor: Theme.of(context).primaryColor.withOpacity(.4)
             ),
-            SizedBox(
-              height: 20,
+          ),
+          Positioned(
+            top: mediaQueryHeight - 150,
+            right: 150,
+            child: CircleAvatar(
+              radius: 130,
+              backgroundColor: Theme.of(context).primaryColor.withOpacity(.4)
             ),
-            LabelWidget(
-              title: "Valor",
-            ),
-            TextFieldWidget(
-              hintText: "Valor",
-              textInputType: TextInputType.number,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            LabelWidget(
-              title: "Categoria do Produto",
-            ),
-            CustomComboboxWidget(
-             items: [
-               Model("01", "Roupas"),
-               Model("02", "Alimentos"),
-               Model("03", "Automotivos"),
-               Model("04","Jogos")],
-             onChange: (item) {
-               print(item.descricao);
-             },
-             itemSelecionado: Model("01", "Roupas"),
-            ),
-            LabelWidget(
-              title: "Tipo produto",
-            ),
-            CustomComboboxWidget(
-             items: [
-               Model("01", "Roupas"),
-               Model("02", "Alimentos"),
-               Model("03", "Automotivos"),
-               Model("04","Jogos")],
-             onChange: (item) {
-               print(item.descricao);
-             },
-             itemSelecionado: Model("01", "Roupas"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: RaisedButton(
-                color: primaryColor,
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text("Salvar", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold))
+          ),
+          Container(
+            height: mediaQueryHeight,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    LabelWidget(
+                      title: "Descrição",
+                    ),
+                    TextFieldWidget(
+                      hintText: "Descrição do Produto",
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    LabelWidget(
+                      title: "Valor",
+                    ),
+                    TextFieldWidget(
+                      hintText: "Valor",
+                      textInputType: TextInputType.number,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    LabelWidget(
+                      title: "Categoria do Produto",
+                    ),
+                    CustomComboboxWidget(
+                    items: [
+                      Model("01", "Roupas"),
+                      Model("02", "Alimentos"),
+                      Model("03", "Automotivos"),
+                      Model("04","Jogos")],
+                    onChange: (item) {
+                      print(item.descricao);
+                    },
+                    itemSelecionado: Model("01", "Roupas"),
+                    ),
+                    LabelWidget(
+                      title: "Tipo produto",
+                    ),
+                    CustomComboboxWidget(
+                    items: [
+                      Model("01", "Roupas"),
+                      Model("02", "Alimentos"),
+                      Model("03", "Automotivos"),
+                      Model("04","Jogos")],
+                    onChange: (item) {
+                      print(item.descricao);
+                    },
+                    itemSelecionado: Model("01", "Roupas"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: RaisedButton(
+                        color: primaryColor,
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text("Salvar", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold))
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ),
+              )
             )
-          ],
-        ),
+          )
+        ],
       )
     );
   }
